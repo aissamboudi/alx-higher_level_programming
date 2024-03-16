cf313d6cf42c4013b1276034ef2986f712164e0a
aissamboudi <aissamboudi@gmail.com>
2024-03-16 16:44:35 -0700

    Fix Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         for arr in matrix:
             for item in arr:
                 if item == arr[-1]:
-                    print("{:d}".format(item),end="")
+                    print("{:d}".format(item), end="")
                 else:
-                    print("{:d}".format(item),end=" ")
-            print()
+                    print("{:d}".format(item), end=" ")
+            print('')
