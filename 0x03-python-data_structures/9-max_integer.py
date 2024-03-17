d9758460a9f8f894668c73a687ac18d27c9597e1
aissamboudi <aissamboudi@gmail.com>
2024-03-16 21:28:02 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    a = 0
+    for i in range(len(my_list)):
+        if my_list[i] > a:
+            a = my_list[i]
+    return a
