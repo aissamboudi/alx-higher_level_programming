cbbf9ef2470b0693751de4f7aac5a1c4ba30781a
aissamboudi <aissamboudi@gmail.com>
2024-03-20 11:21:26 -0700

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1.symmetric_difference(set_2)
+def only_diff_elements(set_1, set_2):
+    if len(set_1):
+        return set_1.symmetric_difference(set_2)
+    elif len(set_2):
+        return set_2.symmetric_difference(set_1)
+    else:
+        return {}
