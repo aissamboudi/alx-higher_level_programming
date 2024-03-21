3ff13f9b8f8d39701dd8cfb97bb3ded873b1cd11
aissamboudi <aissamboudi@gmail.com>
2024-03-21 04:28:36 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
-    num = 0
-    for n in roman_string:
-        num += int(roman.get(n))
-    return num
+    a = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
+    return sum([a[x] if a[x] >= a[roman_string[(i+1) if i < len(roman_string)-1 else i]] else -a[x]for i, x in enumerate(roman_string)])
