bfb217fed11ce98fbadaed1ae37cf765cfc79303
aissamboudi <aissamboudi@gmail.com>
2024-03-20 18:48:16 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        maxval = max(a_dictionary.values(), default=None)
-        if maxval is not None:
-            return next(key for key, value in a_dictionary.items() if value == maxval)
-    else:
-        return None
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
