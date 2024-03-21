508ac7bf5b5b07be8fb677ebc462ff1a424e5402
aissamboudi <aissamboudi@gmail.com>
2024-03-21 06:30:39 -0700

    Update task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,18 @@ def roman_to_int(roman_string):
     r = roman_string
 
     a = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    if not isinstance(roman_string, str):
-        return(0)
+    if not isinstance(r, str):
+        return 0
     else:
         romans_v = []
         for i, x in enumerate(r):
-            if a[x] >= a[r[(i+1) if i < len(r)-1 else i]]:
+            num = 0
+            if i < len(r) - 1:
+                num = i + 1
+            else:
+                num = i
+            if a[x] >= a[r[num]]:
                 romans_v.append(a[x])
             else:
                 romans_v.append(-a[x])
-        return(sum(romans_v))
+        return sum(romans_v)
