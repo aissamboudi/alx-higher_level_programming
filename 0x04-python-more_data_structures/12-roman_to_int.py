1d982a0a140edb6a78d589437c8944db2af9f71a
aissamboudi <aissamboudi@gmail.com>
2024-03-21 04:53:29 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     a = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
-    if roman_string is None or roman_string is not isinstance(roman_string, str):
+    if not isinstance(roman_string, str):
         return 0
     return sum([a[x] if a[x] >= a[roman_string[(i+1) if i < len(roman_string)-1 else i]] else -a[x]for i, x in enumerate(roman_string)])
